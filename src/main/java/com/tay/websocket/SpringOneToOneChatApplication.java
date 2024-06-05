6777828cbffb8dbb51d4b8b0a909cb02a9624d69
tay <phuonggteyy@gmail.com>
2024-06-05 17:19:40 +0700

    init project

---
+++ src/main/java/com/tay/websocket/SpringOneToOneChatApplication.java
@@ -0,0 +1,13 @@
+package com.tay.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneToOneChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneToOneChatApplication.class, args);
+	}
+
+}
